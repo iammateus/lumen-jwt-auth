25b0f278ec2458b0abbb61a383a26ca4d9e4aeba
Mateus Soares da Silva <magnata1997@gmail.com>
2020-01-14 20:16:48 -0300

    Returning JWT after user is successfully logged

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
@@ -31,10 +32,31 @@ class AuthController extends Controller
         $passwordIsValid = Hash::check($request->get("password"), $user->password);
 
         if (!$passwordIsValid) {
-            return response()->json(['message' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+            return response()->json(["message" => "Email or password incorrect"], Response::HTTP_UNAUTHORIZED);
         }
 
-        return response()->json(["message" => "Logged successfully"], Response::HTTP_OK);
+        return $this->responseWithToken($user);
+    }
+
+    public function responseWithToken(User $user)
+    {
+
+        $key = '6w9z$C&F)J@NcRfUjXn2r5u7x!A%D*G-KaPdSgVkYp3s6v9y/B?E(H+MbQeThWmZq4t7w!z%C&F)J@NcRfUjXn2r5u8x/A?D(G-KaPdSgVkYp3s6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-JaNdRfUjXn2r5u8x/A?D(G+KbPeShVkYp3s6v9y$B&E)H@McQfTjWnZq4t7w!z%C*F-JaNdRgUkXp2s5u8x/A?D(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v8y/B?E(G+KbPeShVmYq3t6w9z$C&F)J@McQfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s5v8y/B?E(H+MbQeThWmYq3t6w9z$C&F)J@NcRfUjXn2r4u7x!A%D*G-KaPdSgVkYp3s6v8y/B?E(H+MbQeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&E)H+MbQeThWmZq4';
+
+        $payload = array(
+            "iss" => "intercambiopravaler.com.br",
+            "aud" => "example.com",
+            "iat" => 1356999524,
+            "nbf" => 1357000000,
+            "userId" => $user->id
+        );
+        
+        $jwt = JWT::encode($payload, $key, 'HS512');
+
+        // $decoded = JWT::decode($jwt, $key, array('HS512'));
+        
+        return response()->json([ "data" => [ "token" => print_r($jwt, true) ] ]);
+
     }
 
 }
